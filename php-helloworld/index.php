7fbd81d178af568a590b7efc27ac75ce616105ad
Student User <student@workstation.lab.example.com>
2023-11-08 23:32:39 -0500

    updated app

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,5 +1,5 @@
 <?php
 print "Hello, World! php version is " . PHP_VERSION . "\n";
 print "A change is a coming!\n";
-print "Modified by Kei Okada Oct 31th\n";
+print "Modified by Kei Okada Nov 9th\n";
 ?>
